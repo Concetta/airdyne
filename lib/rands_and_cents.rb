01c6699848a96ee46ae5fbda69ff93b3f2feca3b
Grant Speelman <grant.speelman@ubxd.com>
2013-10-08 08:46:09 +0200

    interpolate strings instead of adding

--- lib/rands_and_cents.rb
+++ lib/rands_and_cents.rb
@@ -9,14 +9,14 @@ def rands_and_cents(rands)
 rand, cents = calculate_rands_and_cents(rands)
 
 	if cents == 0 && rand == 1
-		rand.to_s + " Rand"
+		"#{rand} Rand"
 	elsif cents == 0
-		rand.to_s + " Rands"
+		"#{rand} Rands"
 	elsif rand > 1
-		rand.to_s + " Rands and " + cents.to_s + " cents"
+		"#{rand} Rands and #{cents} cents"
 	elsif rand < 0 
 		" No Rands or cents"
 	else
-		rand.to_s + " Rand and " + cents.to_s + " cents"
+		"#{rand} Rand and #{cents} cents"
 	end
 end 
