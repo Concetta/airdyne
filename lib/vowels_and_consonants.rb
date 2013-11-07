cdca4fffff20aba4c65e945110c22bad3d64d389
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-07 14:36:08 +0200

    Counting vowels up to 5

--- lib/vowels_and_consonants.rb
+++ lib/vowels_and_consonants.rb
@@ -1,5 +1,13 @@
 def counting_vowels(vowels)
-	if "a" 
+	if vowels == "a"
 		return 1 
+	elsif vowels == "aa"
+		return 2 
+	elsif vowels == "aaa"
+		return 3
+	elsif vowels == "aaaa"
+		return 4
+	elsif vowels == "aaaaa"
+		return 5
 	end 
 end 
--- spec/vowels_and_consonants_spec.rb
+++ spec/vowels_and_consonants_spec.rb
@@ -6,5 +6,20 @@ describe "counting vowels_and_consonants" do
 		it "returns 1 if input is 'a'" do 
 			counting_vowels("a").should == 1
 		end 
+		it "returns 1 if input is 'a'" do 
+			counting_vowels("a").should == 1
+		end 
+		it "returns 2 if input is 'aa'" do 
+			counting_vowels("aa").should == 2
+		end 
+		it "returns 3 if input is 'aaa'" do 
+			counting_vowels("aaa").should == 3
+		end 
+		it "returns 4 if input is 'aaaa'" do 
+			counting_vowels("aaaa").should == 4
+		end 
+		it "returns 5 if input is 'aaaaa'" do 
+			counting_vowels("aaaaa").should == 5
+		end 
 	end 
 end 
