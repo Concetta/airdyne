49b74a7ac9a55ec266d815ee46e28c86acdd615d
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-12 21:13:32 +0200

    Refactor to have current board showing

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -7,39 +7,75 @@ def play_on_board(current_board, next_play)
 	 		current_board[next_play * 2 - 2] = "x"
 	 		current_board
 	 	elsif current_board == "x| |o|\n | | |\n | | |\n"
-	 		current_board[2] = "x"
+	 		current_board[next_play * 2 - 2] = "x"
 	 		current_board
-	 	else 
+	 	elsif current_board == "x| | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2 - 2] = "o"
 	 		current_board
+	 	elsif current_board == " |x| |\n | | |\n | | |\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board	
+	 	elsif current_board == " | |x|\n | | |\n | | |\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board
+	 	elsif current_board == " | | |\nx| | |\n | | |\n"
+	 	 	current_board[next_play * 2 - 2] = "o"
+	 		current_board		
+	 	elsif current_board == " | | |\n |x| |\n | | |\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board
+	 	elsif current_board == " | | |\n | |x|\n | | |\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board	 			 		
+	 	elsif current_board == " | | |\n | | |\nx| | |\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board
+	 	elsif current_board == " | | |\n | | |\n |x| |\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board	
+	 	elsif current_board == " | | |\n | | |\n | |x|\n"
+	 		current_board[next_play * 2 - 2] = "o"
+	 		current_board	
 	 	end 
 	 elsif next_play >= 4 && next_play < 7
 	 	if current_board == " | | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2 - 1] = "x"
 	 		current_board
 	 	elsif current_board == "x|o| |\n | | |\n | | |\n"
-	 			current_board[next_play * 2 - 1] = "x"
-	 			current_board
+	 		current_board[next_play * 2 - 1] = "x"
+	 		current_board
 	 	elsif current_board == "x| |o|\n | | |\n | | |\n"
-	 				current_board[next_play * 2 - 1] = "x"
-	 				current_board
-	 	else 
+	 		current_board[next_play * 2 - 1] = "x"
+	 		current_board
+	 	elsif current_board == "x| | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2 - 1] = "o"
 	 		current_board
+	 	elsif current_board == " |x| |\n | | |\n | | |\n"
+	 		current_board[next_play * 2 - 1] = "o"
+	 		current_board
+	 	elsif current_board == " | |x|\n | | |\n | | |\n"
+	 	 	current_board[next_play * 2 - 1] = "o"
+	 		current_board		
 	 	end 
 	 elsif next_play >= 7
 	 	if current_board == " | | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2] = "x"
 	 		current_board
 	 	elsif current_board == "x|o| |\n | | |\n | | |\n"
-	 			current_board[next_play * 2] = "x"
-	 			current_board
+	 		current_board[next_play * 2] = "x"
+	 		current_board
 	 	elsif current_board == "x| |o|\n | | |\n | | |\n"
 	 		current_board[next_play * 2] = "x"
 	 		current_board
-	 	else
+	 	elsif current_board == "x| | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2] = "o"
 	 		current_board 
+	 	elsif current_board == " |x| |\n | | |\n | | |\n"
+	 		current_board[next_play * 2] = "o"
+	 		current_board 
+	 	elsif current_board == " | |x|\n | | |\n | | |\n"
+	 	 	current_board[next_play * 2] = "o"
+	 		current_board 	 	 		
 	 	end 
 	 end
 end 	 	
