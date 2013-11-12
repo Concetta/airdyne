c13adf253e023e7a4b6e20d1d79edff6aff7dbd4
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-12 14:55:22 +0200

    Added third play of x with xo is play 1 and 2

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -3,6 +3,9 @@ def play_on_board(current_board, next_play)
 		if current_board == " | | |\n | | |\n | | |\n"
 			current_board[next_play * 2 - 2] = "x"
 	 		current_board
+	 	elsif current_board == "x|o| |\n | | |\n | | |\n"
+	 		current_board[4] = "x"
+	 		current_board
 	 	else 
 	 		current_board[next_play * 2 - 2] = "o"
 	 		current_board
@@ -10,7 +13,18 @@ def play_on_board(current_board, next_play)
 	 elsif next_play >= 4 && next_play < 7
 	 	if current_board == " | | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2 - 1] = "x"
-	 		current_board	 		
+	 		current_board
+	 	elsif current_board == "x|o| |\n | | |\n | | |\n"
+	 		if next_play == 4
+	 			current_board[7] = "x"
+	 			current_board
+	 		elsif next_play == 5
+	 			current_board[9] = "x"
+	 			current_board
+	 		elsif next_play == 6
+	 			current_board[11] = "x"
+	 			current_board
+	 		end 
 	 	else 
 	 		current_board[next_play * 2 - 1] = "o"
 	 		current_board
@@ -19,6 +33,17 @@ def play_on_board(current_board, next_play)
 	 	if current_board == " | | |\n | | |\n | | |\n"
 	 		current_board[next_play * 2] = "x"
 	 		current_board
+	 	elsif current_board == "x|o| |\n | | |\n | | |\n"
+	 		if next_play == 7
+	 			current_board[14] = "x"
+	 			current_board
+	 		elsif next_play == 8 
+	 			current_board[16] = "x"
+	 			current_board
+	 		elsif next_play == 9
+	 			current_board[18] = "x"
+	 			current_board  
+	 		end 
 	 	else
 	 		current_board[next_play * 2] = "o"
 	 		current_board 
