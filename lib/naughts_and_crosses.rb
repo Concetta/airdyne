cfb0b94643aa9f391dce757d12c2a51da57d7dba
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-12 13:33:33 +0200

    Refactor next play 1 & next play 2

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -1,76 +1,28 @@
 def play_on_board(current_board, next_play)
-	if next_play == 1
+	if next_play >= 1 && next_play < 4
 		if current_board == " | | |\n | | |\n | | |\n"
-			current_board[0] = "x"
-	 		current_board
-	 	else current_board[2] == "x"
-	 		current_board[0] = "o"
-	 		current_board
-	 	end 
-	elsif next_play == 2
-		if current_board == " | | |\n | | |\n | | |\n"
-			current_board[2] = "x"
-	 		current_board
-		else 	 		
-			current_board[2] = "o"
-	 		current_board
-		end 
-	 elsif next_play == 3
-	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		current_board[4] = "x"
+			current_board[next_play * 2 - 2] = "x"
 	 		current_board
 	 	else 
-	 		current_board[4] = "o"
+	 		current_board[next_play * 2 - 2] = "o"
 	 		current_board
 	 	end 
-	 elsif next_play == 4
+	 elsif next_play >= 4 && next_play < 7
 	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		current_board[7] = "x"
+	 		current_board[next_play * 2 - 1] = "x"
 	 		current_board	 		
 	 	else 
-	 		current_board[7] = "o"
+	 		current_board[next_play * 2 - 1] = "o"
 	 		current_board
 	 	end 
-	 elsif next_play == 5
+	 elsif next_play >= 7
 	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		 current_board[9] = "x"
-	 		current_board	
-	 	else 
-	 		current_board[9] = "o"
-	 		current_board
-	 	end 
-	 elsif next_play == 6
-	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		current_board[11] = "x"
-	 		current_board
-	 	else 
-	 		current_board[11] = "o"
-	 		current_board
-	 	end 
-	 elsif next_play == 7
-	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		current_board[14] = "x"
+	 		current_board[next_play * 2] = "x"
 	 		current_board
 	 	else
-	 		current_board[14] = "o"
+	 		current_board[next_play * 2] = "o"
 	 		current_board 
 	 	end 
-	 elsif next_play == 8
-	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		current_board[16] = "x"
-	 		current_board
-	 	else 
-	 		current_board[16] = "o"
-	 		current_board
-	 	end 
-	 elsif next_play == 9
-	 	if current_board == " | | |\n | | |\n | | |\n"
-	 		current_board[18] = "x"
-	 		current_board
-	 	else 
-	 		current_board[18] = "o"
-	 		current_board
-	 	end
 	 end
 end 	 	
 
