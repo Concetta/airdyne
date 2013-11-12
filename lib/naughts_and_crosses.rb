97793f8dd6cb7b4b187fffaf7a4d48448d5e0f76
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-12 08:45:28 +0200

    Next_play 1 & Next_play 2 Refactor

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -1,77 +1,78 @@
 def play_on_board(current_board, next_play)
-	 if current_board == " | | |\n | | |\n | | |\n"
-	 		if next_play == 1
-	 			current_board[0] = "x"
-	 			current_board
-	 		elsif next_play == 2
-	 			current_board[2] = "x"
-	 			current_board
-	 		elsif next_play == 3
-	 			current_board[4] = "x"
-	 			current_board
-	 		elsif next_play == 4
-	 			current_board[7] = "x"
-	 			current_board
-	 		elsif next_play == 5
-	 			current_board[9] = "x"
-	 			current_board
-	 		elsif next_play == 6
-	 			current_board[11] = "x"
-	 			current_board
-	 		elsif next_play == 7 
-	 			current_board[14] = "x"
-	 			current_board
-	 		elsif next_play == 8 
-	 			current_board[16] = "x"
-	 			current_board
-			elsif next_play == 9 
-				current_board[18] = "x"
-	 			current_board
-	 		end 
-	 
-	 elsif next_play == 2
-	 	if current_board[0] == "x"
-	 		current_board[2] = "o"
+	if next_play == 1
+		if current_board == " | | |\n | | |\n | | |\n"
+			current_board[0] = "x"
 	 		current_board
-	 	elsif current_board[4] == "x" 
-	 		current_board[2] = "o"
-	 		current_board
-	 	elsif current_board[7] == "x"
-	 		current_board[2] = "o"
-	 		current_board
-	 	elsif current_board[9] == "x"
-	 		current_board[2] = "o"
-	 		current_board
-	 	elsif current_board[11] == "x"
-	 		current_board[2] = "o"
-	 		current_board
-	 	elsif current_board[14] == "x"
-	 		current_board[2] = "o"
-	 		current_board
-	 	elsif current_board[16] == "x"
-	 		current_board[2] = "o"
-	 		current_board
-	 	elsif current_board[18] == "x"
-	 		current_board[2] = "o"
+	 	else current_board[2] == "x"
+	 		current_board[0] = "o"
 	 		current_board
 	 	end 
-	 
+	elsif next_play == 2
+		if current_board == " | | |\n | | |\n | | |\n"
+			current_board[2] = "x"
+	 		current_board
+		else 	 		
+			current_board[2] = "o"
+	 		current_board
+		end 
 	 elsif next_play == 3
-	 		"x| |o|\n" + " | | |\n" * 2 
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		current_board[4] = "x"
+	 		current_board
+	 	else 
+	 		current_board[4] = "o"
+	 		current_board
+	 	end 
 	 elsif next_play == 4
-	 		"x| | |\no| | |\n" + " | | |\n"
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		current_board[7] = "x"
+	 		current_board	 		
+	 	else 
+	 		current_board[7] = "o"
+	 		current_board
+	 	end 
 	 elsif next_play == 5
-	 		"x| | |\n |o| |\n" + " | | |\n"
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		 current_board[9] = "x"
+	 		current_board	
+	 	else 
+	 		current_board[9] = "o"
+	 		current_board
+	 	end 
 	 elsif next_play == 6
-	 		"x| | |\n | |o|\n" + " | | |\n"
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		current_board[11] = "x"
+	 		current_board
+	 	else 
+	 		current_board[11] = "o"
+	 		current_board
+	 	end 
 	 elsif next_play == 7
-	 		"x| | |\n" + " | | |\n" + "o| | |\n"
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		current_board[14] = "x"
+	 		current_board
+	 	else
+	 		current_board[14] = "o"
+	 		current_board 
+	 	end 
 	 elsif next_play == 8
-	 		"x| | |\n" + " | | |\n" + " |o| |\n"
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		current_board[16] = "x"
+	 		current_board
+	 	else 
+	 		current_board[16] = "o"
+	 		current_board
+	 	end 
 	 elsif next_play == 9
-	 	 	"x| | |\n" + " | | |\n" + " | |o|\n"
+	 	if current_board == " | | |\n | | |\n | | |\n"
+	 		current_board[18] = "x"
+	 		current_board
+	 	else 
+	 		current_board[18] = "o"
+	 		current_board
+	 	end
 	 end
-end 
+end 	 	
 
 
 
