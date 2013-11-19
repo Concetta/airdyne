fb8b99c3a7cabf6846b46d84f5d7e20b0ecaeac0
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-19 15:40:00 +0200

    Refactor with better wording and simplication

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -1,32 +1,30 @@
-def play_on_board(current_board, next_play, play_sequence)
-	play_x = (play_sequence == 1 or play_sequence == 3 or play_sequence == 5 or
-						play_sequence == 7 or play_sequence == 9)
-	row_one = (next_play >= 1 && next_play < 4)
-	row_two = (next_play >= 4 && next_play < 7)
-	row_three = next_play >= 7
+def play_on_board(current_board, position, play_sequence)
+	play_x = play_sequence.odd?
+	row_one = (position >= 1 && position < 4)
+	row_two = (position >= 4 && position < 7)
+	row_three = position >= 7
 	if row_one
 		if play_x
-			current_board[next_play * 2 - 2] = "x"
-	 		current_board
+	 		play(current_board, position, 2, "x")  
 		else
-	 		current_board[next_play * 2 - 2] = "o"
-	 		current_board 		
+			play(current_board, position, 2, "o")
 	 	end 
 	 elsif row_two
 	 	if play_x
-	 		current_board[next_play * 2 - 1] = "x"
-	 		current_board
+	 		play(current_board,position, 1, "x")  
 	 	else 
-	 		current_board[next_play * 2 - 1] = "o"
-	 		current_board	
+	 		play(current_board,position, 1, "o")
 	 	end 
 	 elsif row_three
 	 	if play_x
-	 		current_board[next_play * 2] = "x"
-	 		current_board
-	 	else 
-	 		current_board[next_play * 2] = "o"
-	 		current_board	 			
+	 		play(current_board,position, 0, "x")  
+	 	else
+	 		play(current_board,position, 0, "o")  			
 	 	end 
 	 end
-end 	 	
+end 
+
+def play(current_board, position, value, x_or_o)
+		current_board[position * 2 - value] = x_or_o
+		current_board
+end 
