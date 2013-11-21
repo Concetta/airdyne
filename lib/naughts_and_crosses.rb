3cc078505bd226f650af613433519fe02b30cdcd
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-21 17:17:13 +0200

    Refactor play_on_board, with a new method check_and_play

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -5,45 +5,25 @@ def play_on_board(current_board, position, play_sequence)
 	row_two = (position >= 4 && position < 7)
 	row_three = (position >= 7)
 	if row_one
-		if current_board[0] != " " && position == 1
-		 	raise 'invalid play'
-		elsif current_board[2] != " " && position == 2
-		 	raise 'invalid play'
-		elsif current_board[4] != " " && position == 3
-		 	raise 'invalid play'
-		elsif play_x
-		 	play(current_board, position, 2, "x") 
-		elsif play_o
-			play(current_board, position, 2, "o")
-		end 
+		check_and_play(current_board, position, 2, play_x, play_o)
 	elsif row_two
-		if current_board[7] != " " && position == 4
-			raise 'invalid play'
-		elsif current_board[9] != " " && position == 5
-			raise 'invalid play'
-		elsif current_board[11] != " " && position == 6
-			raise 'invalid play'
-		elsif play_x
-		 	play(current_board,position, 1, "x")
-		elsif play_o
-		 	play(current_board,position, 1, "o")
-		end 
+		check_and_play(current_board, position, 1, play_x, play_o) 
 	elsif row_three
-		if current_board[14] != " " && position == 7
-			raise 'invalid play'
-		elsif current_board[16] != " " && position == 8
-			raise 'invalid play'
-		elsif current_board[18] != " " && position == 9
-			raise 'invalid play'
-		elsif play_x
-		 	play(current_board,position, 0, "x")
-		elsif play_o
-		 	play(current_board,position, 0, "o")  			
-		end 
+		check_and_play(current_board, position, 0, play_x, play_o) 
 	end
 end 
 
-def play(current_board, position, value, x_or_o)
-		current_board[position * 2 - value] = x_or_o
+def play(current_board, position, row_move, x_or_o)
+		current_board[position * 2 - row_move] = x_or_o
 		current_board 
+end
+
+def check_and_play(current_board, position, row_move, play_x, play_o)
+		if current_board[position * 2 - row_move] != " "
+			raise 'invalid play'
+		elsif play_x
+		 	play(current_board,position, row_move, "x")
+		elsif play_o
+		 	play(current_board,position, row_move, "o")  			
+		end 
 end 
