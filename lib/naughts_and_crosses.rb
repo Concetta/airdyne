f8c518a0c64f42d81d22f3172e7baa05f35bee66
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-22 12:05:01 +0200

    Refactor new method winner

--- lib/naughts_and_crosses.rb
+++ lib/naughts_and_crosses.rb
@@ -18,27 +18,26 @@ def play(current_board, position, row_move, x_or_o)
 		current_board 
 end
 
+def winner(current_board, position, row_move, x_or_o, player_wins)
+		current_board[position * 2 - row_move] = x_or_o
+		current_board + player_wins
+end
+
 def check_and_play(current_board, position, row_move, play_x, play_o)
 		if current_board[position * 2 - row_move] != " "
 			raise 'invalid play'
 		elsif current_board[0..20] == "x|x| |\n |o|o|\n | | |\n" && play_x
-			current_board[4] = "x"
-			current_board + " Player 1 wins"
+			winner(current_board, position, row_move, "x", " Player 1 wins")
 		elsif current_board[0..20] == " | | |\nx|o|o|\nx| | |\n" && play_x
-			current_board[0] = "x"
-			current_board + " Player 1 wins"
+			winner(current_board, position, row_move, "x", " Player 1 wins")
 		elsif current_board[0..20] == "o| | |\n |x|o|\n |x| |\n" && play_x
-			current_board[2] = "x"
-			current_board + " Player 1 wins"	
+			winner(current_board, position, row_move, "x", " Player 1 wins")
 		elsif current_board[0..20] == "o|o| |\n |x|x|\nx| | |\n" && play_o
-			current_board[4] = "o"
-			current_board + " Player 2 wins"
+			winner(current_board, position, row_move, "o", " Player 2 wins")
 		elsif current_board[0..20] == " |x| |\no|x|x|\no| | |\n" && play_o
-			current_board[0] = "o"
-			current_board + " Player 2 wins"
+			winner(current_board, position, row_move, "o", " Player 2 wins")
 		elsif current_board[0..20] == "x| | |\nx|o|x|\n |o| |\n" && play_o
-			current_board[2] = "o"
-			current_board + " Player 2 wins"
+			winner(current_board, position, row_move, "o", " Player 2 wins")
 		elsif play_x
 		 	play(current_board,position, row_move, "x")
 		elsif play_o
