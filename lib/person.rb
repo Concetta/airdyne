e4442e37b7680a3a71286ee5979e58aaa38e6ff9
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-26 14:37:31 +0200

    Add full_name method to show first_name & last_name

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,9 @@
 class Person 
 
-	attr_accessor :name, :age
+	attr_accessor :first_name, :last_name, :age
+
+	def full_name
+	 @first_name + " " + @last_name
+	end 
 
 end 
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,10 +3,10 @@ require './lib/person.rb'
 
 describe Person do 
 
-	it "can set name" do
+	it "can set first_name" do
 		person1 = Person.new
-		person1.name = "Grant"
-		person1.name.should == "Grant"
+		person1.first_name = "Grant"
+		person1.first_name.should == "Grant"
 	end
 
 	it "can set age" do 
@@ -15,4 +15,16 @@ describe Person do
 		person1.age.should == "30"
 	end 
 
+	it "can set last_name" do 
+		person1 = Person.new
+		person1.last_name = "Speelman"
+		person1.last_name.should == "Speelman"
+	end 
+
+	it "gives first name & last name" do 
+		person1 = Person.new
+		person1.first_name = "Grant"
+		person1.last_name = "Speelman"
+		person1.full_name.should == "Grant Speelman"	
+	end 
 end 
