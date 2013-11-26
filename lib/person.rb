4db175c11ea91c691ccffb0fc5fdd5a6d286b1d1
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-26 16:17:06 +0200

    Add age_category when age is given

--- lib/person.rb
+++ lib/person.rb
@@ -6,4 +6,23 @@ class Person
 	 @first_name + " " + @last_name
 	end 
 
+	def age_category
+		category = @age.to_i
+		if category >= 0 && category <= 1
+			"Baby"
+		elsif category >= 2 && category <= 3
+			"Toddler"
+		elsif category >= 4 && category <= 12
+			"Child"
+		elsif category >= 13 && category <= 18
+			"Teenager"
+		elsif category >= 19 && category <= 29
+			"Young Adult"
+		elsif category >= 30 && category <= 64
+			"Adult"
+		else 
+			"Pensioner"
+		end 
+	end 
+
 end 
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -26,5 +26,53 @@ describe Person do
 		person1.first_name = "Grant"
 		person1.last_name = "Speelman"
 		person1.full_name.should == "Grant Speelman"	
+	end
+
+	it "age category is Adult when 30 entered" do 
+		person1 = Person.new 
+		person1.age = "30"
+		person1.age_category.should == "Adult"
+	end 
+
+	it "age category is Baby when 0 entered" do 
+		person1 = Person.new 
+		person1.age = "0"
+		person1.age_category.should == "Baby"
+	end 
+
+	it "age category is Baby when 1 entered" do 
+		person1 = Person.new 
+		person1.age = "1"
+		person1.age_category.should == "Baby"
+	end 
+
+	it "age category is Toddler when 2 entered" do 
+		person1 = Person.new 
+		person1.age = "2"
+		person1.age_category.should == "Toddler"
+	end 
+
+	it "age category is Child when 6 entered" do 
+		person1 = Person.new 
+		person1.age = "6"
+		person1.age_category.should == "Child"
+	end 
+
+	it "age category is Teenager when 16 entered" do 
+		person1 = Person.new 
+		person1.age = "16"
+		person1.age_category.should == "Teenager"
+	end 
+
+	it "age category is Teenager when 16 entered" do 
+		person1 = Person.new 
+		person1.age = "25"
+		person1.age_category.should == "Young Adult"
+	end 
+
+	it "age category is Pensioner when 67 entered" do 
+		person1 = Person.new 
+		person1.age = "67"
+		person1.age_category.should == "Pensioner"
 	end 
 end 
