86dec2d98ed488c4fabc917dc50ecd5df9a35f11
Concetta Isolano <concetta.isolano@gmail.com>
2013-11-26 13:44:46 +0200

    Add accessor(read & write) for Person name & age

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,5 +2,17 @@ require 'rspec'
 require './lib/person.rb'
 
 describe Person do 
-	
+
+	it "can set name" do
+		person1 = Person.new
+		person1.name = "Grant"
+		person1.name.should == "Grant"
+	end
+
+	it "can set age" do 
+		person1 = Person.new 
+		person1.age = "30"
+		person1.age.should == "30"
+	end 
+
 end 
