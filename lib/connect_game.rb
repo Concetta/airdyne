823e17c3315a3ba4fdce3a34ac0f61cdd845388c
Concetta Isolano <concetta.isolano@gmail.com>
2013-12-03 12:12:32 +0200

    Add play in row 1 till full

--- lib/connect_game.rb
+++ lib/connect_game.rb
@@ -8,8 +8,51 @@ class ConnectGame
 	end 
 
 	def play(row)
-		@board[20] = "R"
-		@board
-		@whose_turn = "Player Blue"
+		if row == 1 
+			if @board[20] == "." && @whose_turn == "Player Red"
+				@board[20] = "R"
+				@board
+			elsif @board[15] == "." && @whose_turn == "Player Blue"
+				@board[15] = "B"
+				@board
+			elsif @board[15] == "." && @whose_turn == "Player Red"
+				@board[15] = "R"
+				@board
+			elsif @board[10] == "." && @whose_turn == "Player Red"
+				@board[10] = "R"
+				@board
+			elsif @board[5] == "." && @whose_turn == "Player Blue"
+				@board[5] = "B"
+				@board
+			elsif @board[5 * 0] == "." && @whose_turn == "Player Red"
+				@board[5 * 0] = "R"
+				@board
+			end 
+		elsif row == 2
+			if @board[21] == "." && @whose_turn == "Player Red"
+				@board[21] = "R"
+				@board
+			elsif @board[21] == "." && @whose_turn == "Player Blue"
+				@board[21] = "B"
+				@board
+			elsif @board[16] == "." && @whose_turn == "Player Blue"
+				@board[16] = "B"
+				@board
+			end 
+		elsif row == 3
+			@board[22] = "R"
+			@board
+		elsif row == 4
+			@board[23] = "R"
+			@board
+		elsif row == 5
+			@board[24] = "R"
+			@board
+		end  
+		if @whose_turn == "Player Red"
+			@whose_turn = "Player Blue"
+		else 
+			@whose_turn = "Player Red"
+		end
 	end 
 end 
--- spec/connect_game_spec.rb
+++ spec/connect_game_spec.rb
@@ -3,25 +3,153 @@ require './lib/connect_game.rb'
 
 describe ConnectGame do
 
-	context "New" do  
+	before :each do 
+		@game = ConnectGame.new 
+	end 
+
+	context "New" do 
 		it "start with an empty board" do 
-			game = ConnectGame.new
-			game.board.should == 
+			@game.board.should == 
 			"........................."
 		end 
 
 		it "start with an empty board and Red Players turn" do 
-			game = ConnectGame.new
-			game.whose_turn.should == "Player Red"
+			@game.whose_turn.should == "Player Red"
 		end 
 	end
 
 	context "Play" do
-		it "Player Red plays first in row 1" do 
-			game = ConnectGame.new
-			game.play(1)
-			game.board.should == "....................R...."
-			game.whose_turn.should == "Player Blue"
+		context "first move" do 
+			it "Player Red plays in row 1" do 
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+			end
+
+			it "Player Red plays in row 2" do 
+				@game.play(2)
+				@game.board.should == ".....................R..."
+				@game.whose_turn.should == "Player Blue"
+			end 
+
+			it "Player Red plays in row 3" do 
+				@game.play(3)
+				@game.board.should == "......................R.."
+				@game.whose_turn.should == "Player Blue"
+			end 
+
+			it "Player Red plays in row 4" do
+				@game.play(4)
+				@game.board.should == ".......................R."
+				@game.whose_turn.should == "Player Blue"
+			end
+
+			it "Player Red plays in row 5" do 
+				@game.play(5)
+				@game.board.should == "........................R"
+				@game.whose_turn.should == "Player Blue"
+			end
+		end 
+
+		context "second move" do 
+			it "Player Blue plays in row 1 with Red in row 1" do
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(1)
+				@game.board.should == "...............B....R...."
+				@game.whose_turn.should == "Player Red"
+			end
+
+			it "Player Blue plays in row 2 with Red in row 1" do 
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(2)
+				@game.board.should == "....................RB..."
+				@game.whose_turn.should == "Player Red"			
+			end 
+		end  
+
+		context "third move" do 
+			it "Player Red plays in row 1 with Red and Blue in row 1" do
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(1)
+				@game.board.should == "...............B....R...."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "..........R....B....R...."
+				@game.whose_turn.should == "Player Blue"
+			end 
+
+			it "Player Red plays in row 1 with Red in row 1 and Blue in row 2" do
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(2)
+				@game.board.should == "....................RB..."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "...............R....RB..."
+				@game.whose_turn.should == "Player Blue"
+			end 
+		end
+
+		context "forth move" do 
+			it "Player Blue plays in row 1 with Red and Blue in Row 1" do 
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(1)
+				@game.board.should == "...............B....R...."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "..........R....B....R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(1)
+				@game.board.should == ".....B....R....B....R...."
+				@game.whose_turn.should == "Player Red"
+			end
+		end 
+
+		context "fifth move" do 
+			it "Player Red plays in row 1 with Blue and Red in row 1" do 
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(1)
+				@game.board.should == "...............B....R...."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "..........R....B....R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(1)
+				@game.board.should == ".....B....R....B....R...."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "R....B....R....B....R...."
+				@game.whose_turn.should == "Player Blue"
+			end 
+
+			it "Player Red plays in row 1 with Blue in row 2 and Red in row 1" do 
+				@game.play(1)
+				@game.board.should == "....................R...."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(2)
+				@game.board.should == "....................RB..."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "...............R....RB..."
+				@game.whose_turn.should == "Player Blue"
+				@game.play(2)
+				@game.board.should == "...............RB...RB..."
+				@game.whose_turn.should == "Player Red"
+				@game.play(1)
+				@game.board.should == "..........R....RB...RB..."
+				@game.whose_turn.should == "Player Blue"
+			end 
 		end 
 	end 
 end 
