d209a36673c76fdeb731cca40ff6a8240ac7520e
Concetta Isolano <concetta.isolano@gmail.com>
2013-12-04 17:07:04 +0200

    Refactor check_horizontal_win

--- lib/connect_game.rb
+++ lib/connect_game.rb
@@ -22,27 +22,21 @@ class ConnectGame
 	end 
 
 	def check_horizontal_win(r_or_b, who_wins)
-		if @board[0] == r_or_b && @board[1] == r_or_b && @board[2] == r_or_b && @board[3] == r_or_b 
-			who_wins	
-		elsif @board[1] == r_or_b && @board[2] == r_or_b && @board[3] == r_or_b && @board[4] == r_or_b 
-			who_wins	
-		elsif @board[5] == r_or_b && @board[6] == r_or_b && @board[7] == r_or_b && @board[8] == r_or_b 
-			who_wins	
-		elsif @board[6] == r_or_b && @board[7] == r_or_b && @board[8] == r_or_b && @board[9] == r_or_b 
-			who_wins	
-		elsif @board[10] == r_or_b && @board[11] == r_or_b && @board[12] == r_or_b && @board[13] == r_or_b 
-			who_wins	
-		elsif @board[11] == r_or_b && @board[12] == r_or_b && @board[13] == r_or_b && @board[14] == r_or_b
-			who_wins				
-		elsif @board[15] == r_or_b && @board[16] == r_or_b && @board[17] == r_or_b && @board[18] == r_or_b
-			who_wins	
-		elsif @board[16] == r_or_b && @board[17] == r_or_b && @board[18] == r_or_b && @board[19] == r_or_b 
-			who_wins				
-		elsif @board[20] == r_or_b && @board[21] == r_or_b && @board[22] == r_or_b && @board[23] == r_or_b 
-			who_wins
-		elsif @board[21] == r_or_b && @board[22] == r_or_b && @board[23] == r_or_b && @board[24] == r_or_b 
-			who_wins
+		the_winner = nil 
+		(0.upto(1)).each do |x|
+			if @board[0 + x] == r_or_b && @board[1 + x] == r_or_b && @board[2 + x] == r_or_b && @board[3 + x] == r_or_b 
+				the_winner = who_wins
+			elsif @board[5 + x] == r_or_b && @board[6 + x] == r_or_b && @board[7 + x] == r_or_b && @board[8 + x] == r_or_b 
+				the_winner = who_wins
+			elsif @board[10 + x] == r_or_b && @board[11 + x] == r_or_b && @board[12 + x] == r_or_b && @board[13 + x] == r_or_b 
+				the_winner = who_wins	
+			elsif @board[15 + x] == r_or_b && @board[16 + x] == r_or_b && @board[17 + x] == r_or_b && @board[18 + x] == r_or_b
+				the_winner = who_wins		
+			elsif @board[20 + x] == r_or_b && @board[21 + x] == r_or_b && @board[22 + x] == r_or_b && @board[23 + x] == r_or_b 
+				the_winner = who_wins	
+			end 
 		end 
+		the_winner
 	end 
 
 	def check_diagonal_win(r_or_b, who_wins)
